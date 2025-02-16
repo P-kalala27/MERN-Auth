db6895f6c8555e4b8e139389c5980d91ad328d1d
P-kalala27 <jpmulyanga@gmail.com>
2025-02-16 23:48:14 +0100

    adding some new file such as dockerfiles, and automation with github action

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
 //connection a la base de donnees avec mongoose
 
 import mongoose from "mongoose";
+const MONGODB_URL="mongodb+srv://princekalala29:8U6fD5KSfYx71fGI@cluster0.7fodo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const connectDB = async () =>{
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URL)
+        const db = await mongoose.connect(MONGODB_URL)
         console.log(`mongodb connected to ${db.connection.host}`);
     } catch (error) {
         console.error(`mongodb connection error: ${error.message}`);
